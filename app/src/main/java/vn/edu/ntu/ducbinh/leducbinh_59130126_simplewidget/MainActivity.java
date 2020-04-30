58695aafbf9b5a852ddea5aea7b873f90f177ebf
luvastar00 <62916552+luvastar00@users.noreply.github.com>
2020-04-30 11:11:48 +0700

    Fix bài

--- app/src/main/java/vn/edu/ntu/ducbinh/leducbinh_59130126_simplewidget/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/ducbinh/leducbinh_59130126_simplewidget/MainActivity.java
@@ -2,11 +2,13 @@ package vn.edu.ntu.ducbinh.leducbinh_59130126_simplewidget;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -14,8 +16,9 @@ public class MainActivity extends AppCompatActivity {
 
     RadioGroup grbt;
     Button btxn;
-    CheckBox ckb;
-    EditText ten,date;
+    EditText ten,datee,stk;
+    RadioButton nu,nam;
+    CheckBox xp,nghe,mot,bep,cafe;
 
 
     @Override
@@ -27,6 +30,18 @@ public class MainActivity extends AppCompatActivity {
     }
     private void addViews()
     {
+    ten = findViewById(R.id.ten);
+    datee = findViewById(R.id.datee);
+    grbt = findViewById(R.id.grbt);
+    stk = findViewById(R.id.stk);
+    btxn = findViewById(R.id.btxn);
+    nu = findViewById(R.id.nu);
+    nam = findViewById(R.id.nam);
+    xp = findViewById(R.id.xp);
+    cafe = findViewById(R.id.cafe);
+    mot = findViewById(R.id.mot);
+    bep = findViewById(R.id.bep);
+    nghe = findViewById(R.id.nghe);
 
     }
     private  void addEvent()
@@ -40,6 +55,45 @@ btxn.setOnClickListener(new View.OnClickListener() {
     }
     private void xacnhan()
     {
-        Toast.makeText(getApplicationContext(), ".....", Toast.LENGTH_SHORT).show();
+    int sex = this.grbt.getCheckedRadioButtonId();
+            String name = ten.getText().toString().trim();
+            String date = datee.getText().toString().trim();
+            RadioButton grbt = this.findViewById(sex);
+            String st ="";
+            if(nghe.isChecked()){
+                if (st.length()>0){
+                    st+= ",";
+                }
+                st += "Nghe Nhạc";
+            }
+             if(xp.isChecked()){
+            if (st.length()>0){
+                st+= ",";
+            }
+            st += "Xem Phim";
+        }
+             if(bep.isChecked()){
+            if (st.length()>0){
+                st+= ",";
+            }
+            st += "Vào Bếp Nấu Ăn";
+        }
+             if(mot.isChecked()){
+            if (st.length()>0){
+                st+= ",";
+            }
+            st += "Ở nhà một mình";
+        }
+             if(cafe.isChecked()){
+            if (st.length()>0){
+                st+= ",";
+            }
+            st += "Đi cafe với bạn";
+        }
+             st +=",";
+             st += stk.getText().toString().trim();
+         String xuat = name + "\nNgaySinh : " + date + "\nGiới Tính : " + grbt.getText() + "\nSở Thích :" + st ;
+        Toast.makeText(getApplicationContext(), xuat, Toast.LENGTH_SHORT).show();
     }
+
 }
